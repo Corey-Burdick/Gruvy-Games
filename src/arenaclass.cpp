47426e124d9af9aa0265613c886403566ef891d7
Corey-Burdick <coreyburdickwork@gmail.com>
2025-09-06 21:41:16 -0400

    added more details to arena.

--- src/arenaclass.cpp
+++ src/arenaclass.cpp
@@ -11,4 +11,12 @@ void Arena::Update() {
 void Arena::Draw() {
   DrawRectangle(0, 0, width / 2, height, BLUE);
   DrawRectangle(width / 2, 0, width / 2, height, RED);
+  DrawCircle(0, height / 2, height * 0.75 / 2, BLACK);
+  DrawCircle(0, height / 2, height * 0.70 / 2, RED);
+  DrawCircle(width, height / 2, height * 0.75 / 2, BLACK);
+  DrawCircle(width, height / 2, height * 0.70 / 2, BLUE);
+  DrawRectangle(width / 2 - 8, 0, 16, height * 0.25, BLACK);
+  DrawRectangle(width / 2 - 8, height * 0.75, 16, height * 0.25, BLACK);
+  DrawRing(Vector2{(float)width / 2, (float)height / 2}, height * 0.23, height * 0.25, 0, 360, 60, BLACK);
+  DrawFPS(0, 0);
 }
