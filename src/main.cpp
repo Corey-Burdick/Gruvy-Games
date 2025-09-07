1e2c45d39771fa10e74c20549b9905e26e4f1191
Corey-Burdick <coreyburdickwork@gmail.com>
2025-09-07 08:41:13 -0400

    Added launch options to toggle fullscreen and high resolution mode on launch.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,36 @@
 #include <raylib.h>
+#include <cstring>
 
 #include "gameclass.h"
 
-int main() {
+int main(int argc, char* argv[]) {
   int windowWidth = 1280;
   int windowHeight = 720;
+  bool fullscreen = false;
+
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      if (argv[i][0] == '-') {
+        for (int j = 1; j < strlen(argv[i]); j++) {
+          switch (argv[i][j]) {
+            case 'f':
+              fullscreen = true;
+              break;
+            case 'h':
+              windowWidth = 1920;
+              windowHeight = 1080;
+              break;
+          }
+        }
+      }
+    }
+  }
   
   InitWindow(windowWidth, windowHeight, "Gruvy Games");
-  ToggleFullscreen();
+
+  if (fullscreen) {
+    ToggleFullscreen();
+  }
 
   Game game = Game();
 
@@ -21,5 +44,8 @@ int main() {
 
     EndDrawing();
   }
-  ToggleFullscreen();
+
+  if (fullscreen) {
+    ToggleFullscreen();
+  }
 }
