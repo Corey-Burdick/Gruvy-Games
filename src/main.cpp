edf9b0d2643ffd2d4eaecc5b572ef1ce56156284
Corey-Burdick <coreyburdickwork@gmail.com>
2025-09-06 20:56:51 -0400

    main.cpp now calls the game class and renders the window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include <raylib.h>
 
+#include "gameclass.h"
+
 int main() {
-   
+  Game game = Game();   
+  int windowWidth = 1280;
+  int windowHeight = 720;
+  
+  InitWindow(windowWidth, windowHeight, "Gruvy Games");
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(WHITE);
+
+    game.Update();
+
+    game.Draw();
+
+    EndDrawing();
+  }
 }
