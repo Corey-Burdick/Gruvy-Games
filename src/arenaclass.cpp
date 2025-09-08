15f66140d9caaf01d650487815ce32570a4baaa7
Corey-Burdick <coreyburdickwork@gmail.com>
2025-09-08 14:04:33 -0400

    Fixed visible disconnect between the center ring and the center lines in the arena.

--- src/arenaclass.cpp
+++ src/arenaclass.cpp
@@ -17,6 +17,6 @@ void Arena::Draw() {
   DrawCircle(width, height / 2, height * 0.70 / 2, GRUVBOX_DARKBLUE);
   DrawRectangle(width / 2 - 8, 0, 16, height * 0.25, GRUVBOX_FG);
   DrawRectangle(width / 2 - 8, height * 0.75, 16, height * 0.25, GRUVBOX_FG);
-  DrawRing(Vector2{(float)width / 2, (float)height / 2}, height * 0.23, height * 0.25, 0, 360, 60, GRUVBOX_FG);
+  DrawRing(Vector2{(float)width / 2, (float)height / 2}, height * 0.25, height * 0.28, 0, 360, 60, GRUVBOX_FG);
   DrawFPS(0, 0);
 }
