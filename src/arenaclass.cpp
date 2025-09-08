2db0fdf66d6ad939e1caaab621f030749e6372fd
Corey-Burdick <coreyburdickwork@gmail.com>
2025-09-07 21:06:51 -0400

    Changed colors for higher contrast.

--- src/arenaclass.cpp
+++ src/arenaclass.cpp
@@ -9,12 +9,12 @@ void Arena::Update() {
 }
 
 void Arena::Draw() {
-  DrawRectangle(0, 0, width / 2, height, GRUVBOX_BLUE);
-  DrawRectangle(width / 2, 0, width / 2, height, GRUVBOX_RED);
+  DrawRectangle(0, 0, width / 2, height, GRUVBOX_DARKBLUE);
+  DrawRectangle(width / 2, 0, width / 2, height, GRUVBOX_DARKRED);
   DrawCircle(0, height / 2, height * 0.75 / 2, GRUVBOX_FG);
-  DrawCircle(0, height / 2, height * 0.70 / 2, GRUVBOX_RED);
+  DrawCircle(0, height / 2, height * 0.70 / 2, GRUVBOX_DARKRED);
   DrawCircle(width, height / 2, height * 0.75 / 2, GRUVBOX_FG);
-  DrawCircle(width, height / 2, height * 0.70 / 2, GRUVBOX_BLUE);
+  DrawCircle(width, height / 2, height * 0.70 / 2, GRUVBOX_DARKBLUE);
   DrawRectangle(width / 2 - 8, 0, 16, height * 0.25, GRUVBOX_FG);
   DrawRectangle(width / 2 - 8, height * 0.75, 16, height * 0.25, GRUVBOX_FG);
   DrawRing(Vector2{(float)width / 2, (float)height / 2}, height * 0.23, height * 0.25, 0, 360, 60, GRUVBOX_FG);
